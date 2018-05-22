e0774a88f7fe677f6507daf5280d3c7de67e35b5
ben300694 <werbung_ben@live.de>
2018-05-22 16:34:33 +0200

    Added bounding_box_netlength

--- main.cpp
+++ main.cpp
@@ -23,6 +23,32 @@ std::ostream &operator<<(std::ostream &out, const std::vector<T> &v) {
     return out;
 }
 
+long bounding_box(const std::vector<std::vector <long>> listofpoints){
+    long xmin=listofpoints[0][0];
+    long xmax=listofpoints[0][0];
+    long ymin=listofpoints[0][1];
+    long ymax=listofpoints[0][1];
+
+    for(auto point: listofpoints){
+        if (point[0]<xmin){
+          xmin=point[0];
+        }
+        if (point[0]>xmax){
+            xmax=point[0];
+        }
+        if (point[1]>ymax){
+            ymax=point[0];
+        }
+        if (point[0]<ymin){
+            ymin=point[0];
+        }
+    }
+    long length = xmax-xmin+ymax-ymin;
+    return length;
+}
+
+
+
 int main(int argc, char** argv) {
 
     std::vector<std::vector<long>> list_of_points;
